02cb4b491c08adf05285d9d81e543c2d8791ed60
Kelvin <kelvin.peters@hotmail.com>
2018-10-03 15:46:09 +0200

    Updated Database Diagrams

--- MonitorAPI/MonitorAPI.DAL/Models/Service.cs
+++ MonitorAPI/MonitorAPI.DAL/Models/Service.cs
@@ -3,15 +3,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MonitorAPI.DAL.Models
 {
+    /// <summary>
+    /// The Service class. Which is used as an object.
+    /// </summary>
     public class Service
     {
+        /// <summary>
+        /// Contains different elements which make up a service.
+        /// </summary>
+
+        /// <param>
+        /// Make the <c>Id</c> the primary key.
+        /// </param>
         [Key]
+        /// <value>The Id of a service. <c>int</c></value>
         public int Id { get; set; }
-
+        /// <value>The URL of a service. <c>string</c></value>
         public string Link { get; set; }
-
+        /// <value>The Status of a service. <c>bool</c></value>
         public bool Status { get; set; }
-
+        /// <value>The Timestamp of a service. <c>bool</c></value>
         public string TimeStamp { get; set; }
     }
 }
--- MonitorAPI/MonitorAPI.DAL/Models/Website.cs
+++ MonitorAPI/MonitorAPI.DAL/Models/Website.cs
@@ -3,17 +3,28 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MonitorAPI.DAL.Models
 {
+    /// <summary>
+    /// The Website class. Which is used as an object.
+    /// </summary>
     public class Website
     {
+        /// <summary>
+        /// Contains different elements which make up a Website.
+        /// </summary>
+
+        /// <param>
+        /// Make the <c>Id</c> the primary key.
+        /// </param>
         [Key]
+        /// <value>The Id of a service. <c>int</c></value>
         public int Id { get; set; }
-
+        /// <value>The URL of a service. <c>string</c></value>
         public string Link { get; set; }
-
+        /// <value>The Word which will be used to check the service. <c>string</c></value>
         public string Word { get; set; }
-
+        /// <value>The Status of a service. <c>string</c></value>
         public bool Status { get; set; }
-
+        /// <value>The TimeStamp of a service at the time of the check. <c>string</c></value>
         public string TimeStamp { get; set; }
     }
 }
